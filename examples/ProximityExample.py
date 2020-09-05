82bf3ff82f7e69a98bc465cbf64f40f68d27fae4
leoli51 <leoli51@users.noreply.github.com>
2020-09-05 15:55:35 +0200

    testing examples

--- examples/ProximityExample.py
+++ examples/ProximityExample.py
@@ -5,9 +5,10 @@ import melopero_apds9960 as mp
 def main():
     device = mp.APDS_9960()
 
+    device.enable_all_engines_and_power_up(False)
     device.enable_proximity_engine()
     device.power_up()
-
+    print(device.get_status())
     while True:
         time.sleep(.5)
         print(device.get_proximity_data())
